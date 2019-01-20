1bf7d7beef073f3832915d49d4c29b85102430cf
javier <cardozojavier.c@gmail.com>
2019-01-19 23:01:51 -0300

    Updates

---
+++ precache-manifest.401d1511e2880474e7d7892832f79210.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "184de12588db3157ba07",
+    "url": "/challenge/static/js/runtime~main.184de125.js"
+  },
+  {
+    "revision": "85058a5fb9637f2200ca",
+    "url": "/challenge/static/js/main.85058a5f.chunk.js"
+  },
+  {
+    "revision": "1f78136aea04b227b7bc",
+    "url": "/challenge/static/js/1.1f78136a.chunk.js"
+  },
+  {
+    "revision": "85058a5fb9637f2200ca",
+    "url": "/challenge/static/css/main.183b3458.chunk.css"
+  },
+  {
+    "revision": "40bb175760f3a9092f102130262e2af5",
+    "url": "/challenge/index.html"
+  }
+];
